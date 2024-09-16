a5a5175dbfa7412e7930e6e80e218136154d11fa
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 14:11:42 -0300

    Refactor JwtAuthenticationFilter to handle user authentication and authorization

--- src/main/java/com/gorges/studycardsapi/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/gorges/studycardsapi/filter/JwtAuthenticationFilter.java
@@ -5,16 +5,13 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.gorges.studycardsapi.error.http.NotFound404Exception;
 import com.gorges.studycardsapi.jwt.JwtUtil;
 
 import jakarta.servlet.FilterChain;
@@ -24,54 +21,41 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    @Autowired
-    JwtUtil jwtUtil;
 
     @Autowired
-    UserDetailsService userDetailsService;
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
 
+        final String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        try {
-            final String jwt = authHeader.substring(7);
-            final String userEmail = jwtUtil.extractEmail(jwt);
+        final String jwt = authHeader.substring(7);
+        final String userEmail = jwtUtil.extractEmail(jwt);
 
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
 
-            if (userEmail != null && authentication == null) {
-                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtUtil.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                if (jwtUtil.isTokenValid(jwt, userDetails)) {
-                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                            userDetails,
-                            jwt,
-                            userDetails.getAuthorities());
-
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
-
-            filterChain.doFilter(request, response);
-
-            // SHOUDLD FIND A BETTER WAY OF HANDLING EXCEPTIONS HERE, GLOBAL EXCEPTION
-            // HANDLER DON'T WORK FOR FILTERS
-        } catch (ServletException servletException) {
-            throw new ServletException(servletException);
-        } catch (UsernameNotFoundException usernameNotFoundException) {
-            throw new NotFound404Exception("User not found");
-        } catch (IOException ioException) {
-            throw new IOException(ioException.getMessage());
         }
+        filterChain.doFilter(request, response);
     }
 }
