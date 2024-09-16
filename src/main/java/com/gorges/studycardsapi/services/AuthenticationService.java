415158d5eddb105dd3bd7dd2deabfd4d9ccc02cd
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 14:23:55 -0300

    Add AuthenticationService for user authentication and registration

---
+++ src/main/java/com/gorges/studycardsapi/services/AuthenticationService.java
@@ -0,0 +1,66 @@
+package com.gorges.studycardsapi.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.gorges.studycardsapi.dto.AuthenticationDtos.LoginDto;
+import com.gorges.studycardsapi.dto.AuthenticationDtos.RegisterDto;
+import com.gorges.studycardsapi.error.http.Unauthorized401Exception;
+import com.gorges.studycardsapi.models.UserEntity;
+import com.gorges.studycardsapi.repositories.UserRepository;
+import com.gorges.studycardsapi.utils.enums.Roles;
+
+@Service
+public class AuthenticationService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public UserEntity signup(@RequestBody RegisterDto registerDto) throws IllegalArgumentException {
+        UserEntity userEntity = registerDto.toEntity();
+        userEntity.setRole(Roles.ROLE_USER);
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity authenticate(LoginDto loginDto) throws AuthenticationException, UsernameNotFoundException {
+        if (isUserAuthenticated()) {
+            throw new Unauthorized401Exception("Logout first before authenticating");
+        }
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginDto.getEmail(),
+                            loginDto.getPassword()));
+        } catch (AuthenticationException e) {
+            throw new Unauthorized401Exception("Invalid email or password");
+        }
+
+        UserEntity user = userRepository.findByEmail(loginDto.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (user.getDeleteAt() != null) {
+            throw new Unauthorized401Exception("Deleted user");
+        }
+
+        userRepository.updateLastLogin(user.getId());
+        return user;
+    }
+
+    private boolean isUserAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getPrincipal() != "anonymousUser";
+    }
+}
