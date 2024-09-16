607c3e68f9545b193c080c1e2d77e038f339036b
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 17:46:28 -0300

    Add CardEntity model

---
+++ src/main/java/com/gorges/studycardsapi/models/CardEntity.java
@@ -0,0 +1,60 @@
+package com.gorges.studycardsapi.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "card")
+public class CardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private String answer;
+
+    @ManyToOne
+    @JoinColumn(name = "collection_id", nullable = false)
+    private CardCollectionEntity collection;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private UserEntity owner;
+
+    @Column(nullable = false)
+    private boolean isPublic;
+
+    @Column(nullable = false)
+    private boolean IsAnswarable;
+
+    @Column(nullable = false)
+    private boolean isMultipleChoice;
+
+    @Column(nullable = false)
+    private boolean isDissertative;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @Column(nullable = true, name = "deleteAt")
+    private LocalDateTime deleteAt;
+}
