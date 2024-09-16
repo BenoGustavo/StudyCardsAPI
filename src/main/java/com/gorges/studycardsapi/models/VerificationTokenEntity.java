56a0814c51eb8d1908673f6839f83dab46e853c6
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 19:18:35 -0300

    Add VerificationTokenEntity model

---
+++ src/main/java/com/gorges/studycardsapi/models/VerificationTokenEntity.java
@@ -0,0 +1,34 @@
+package com.gorges.studycardsapi.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "verification_tokens")
+public class VerificationTokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+}
