23c37e04d8c72fcd9bf4102e285a7260d01fb67c
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 14:53:44 -0300

    Refactor SwaggerConfig to include security requirements and components

--- src/main/java/com/gorges/studycardsapi/config/SwaggerConfig.java
+++ src/main/java/com/gorges/studycardsapi/config/SwaggerConfig.java
@@ -4,9 +4,12 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -31,11 +34,11 @@ public class SwaggerConfig {
                                                 .version("1.0")
                                                 .description("A simple API to manage study cards")
                                                 .termsOfService("http://swagger.io/terms/")
-                                                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-                // .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                // .components(new Components()
-                // .addSecuritySchemes("bearerAuth",
-                // new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                // .scheme("bearer").bearerFormat("JWT")));
+                                                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .components(new Components()
+                                                .addSecuritySchemes("bearerAuth",
+                                                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer").bearerFormat("JWT")));
         }
 }
