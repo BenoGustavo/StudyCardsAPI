8e60ea5f554a278b6f921510cb9a906fb29dff1a
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 14:53:17 -0300

    Refactor AuthenticationDtos to include nested classes for LoginDto and TokenDto

--- src/main/java/com/gorges/studycardsapi/dto/AuthenticationDtos.java
+++ src/main/java/com/gorges/studycardsapi/dto/AuthenticationDtos.java
@@ -1,37 +1,47 @@
 package com.gorges.studycardsapi.dto;
 
-import com.gorges.studycardsapi.models.UserEntity;
+import java.util.Date;
 
 import lombok.Data;
 
 public class AuthenticationDtos {
     @Data
-    public class RegisterDto {
-        private String username;
+    public static class LoginDto {
         private String email;
         private String password;
-        private String confirmPassword;
-
-        public RegisterDto(String username, String email, String password, String confirmPassword) {
-            this.username = username;
-            this.email = email;
-            this.password = password;
-            this.confirmPassword = confirmPassword;
-        }
-
-        public UserEntity toEntity() {
-            UserEntity userEntity = new UserEntity();
-            userEntity.setUsername(this.username);
-            userEntity.setEmail(this.email);
-            userEntity.setPassword(this.password);
-            return userEntity;
-        }
     }
 
     @Data
-    public class LoginDto {
-        private String email;
-        private String password;
-    }
+    public static class TokenDto {
+        private String token;
+        private Date expiresIn;
 
+        private TokenDto(Builder builder) {
+            this.token = builder.token;
+            this.expiresIn = builder.expiresIn;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static class Builder {
+            private String token;
+            private Date expiresIn;
+
+            public Builder token(String token) {
+                this.token = token;
+                return this;
+            }
+
+            public Builder expiresIn(Date expiresIn) {
+                this.expiresIn = expiresIn;
+                return this;
+            }
+
+            public TokenDto build() {
+                return new TokenDto(this);
+            }
+        }
+    }
 }
