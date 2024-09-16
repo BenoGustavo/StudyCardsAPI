456c24f94df8c100211c27eab581777aa0c12b28
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 14:11:18 -0300

    Add UserRepository interface for managing user data

---
+++ src/main/java/com/gorges/studycardsapi/repositories/UserRepository.java
@@ -0,0 +1,30 @@
+package com.gorges.studycardsapi.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gorges.studycardsapi.models.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    @Query("SELECT u FROM UserEntity u WHERE u.deleteAt IS NULL")
+    List<UserEntity> findAllActive();
+
+    @Query("SELECT u FROM UserEntity u WHERE u.id = :id AND u.deleteAt IS NULL")
+    Optional<UserEntity> findByIdActive(UUID id);
+
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmailAndUsername(String email, String username);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserEntity u SET u.lastLogin = CURRENT_TIMESTAMP WHERE u.id = :id")
+    void updateLastLogin(@Param("id") UUID id);
+}
