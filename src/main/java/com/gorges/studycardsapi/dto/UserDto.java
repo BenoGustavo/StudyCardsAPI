f5040d9461e9c7df604d9936f0923cdc95fb0296
Gustavo <gustavoleandrogorges@gmail.com>
2024-09-16 14:53:11 -0300

    Refactor UserDto to include nested classes for User and Register

--- src/main/java/com/gorges/studycardsapi/dto/UserDto.java
+++ src/main/java/com/gorges/studycardsapi/dto/UserDto.java
@@ -1,12 +1,41 @@
 package com.gorges.studycardsapi.dto;
 
+import com.gorges.studycardsapi.models.UserEntity;
 import com.gorges.studycardsapi.utils.enums.Roles;
 
 import lombok.Data;
 
-@Data
 public class UserDto {
-    private String username;
-    private String email;
-    private Roles role;
+    @Data
+    public static class User {
+        private String username;
+        private String email;
+        private Roles role;
+    }
+
+    @Data
+    public static class Register {
+        private String username;
+        private String email;
+        private String password;
+        private String confirmPassword;
+
+        public Register() {
+        }
+
+        public Register(String username, String email, String password, String confirmPassword) {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.confirmPassword = confirmPassword;
+        }
+
+        public UserEntity toEntity() {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(this.username);
+            userEntity.setEmail(this.email);
+            userEntity.setPassword(this.password);
+            return userEntity;
+        }
+    }
 }
