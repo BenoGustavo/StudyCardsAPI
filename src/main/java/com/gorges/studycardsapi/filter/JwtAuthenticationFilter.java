b0152431923360ef2b1a06cf17ad58dfe7823ccd
Gustavo <gustavoleandrogorges@gmail.com>
2024-10-15 16:18:44 -0300

    Refactor JwtAuthenticationFilter to add lazy loading for UserDetailsService

--- src/main/java/com/gorges/studycardsapi/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/gorges/studycardsapi/filter/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.gorges.studycardsapi.filter;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,6 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtUtil jwtUtil;
 
     @Autowired
+    @Lazy
     private UserDetailsService userDetailsService;
 
     @Override
